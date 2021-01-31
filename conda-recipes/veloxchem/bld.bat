ac4a62a01a513e980c2ccf4d95b8bed96bf76cc4
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-01-31 05:56:26 -0800

    Work on Windows package

--- conda-recipes/veloxchem/bld.bat
+++ conda-recipes/veloxchem/bld.bat
@@ -6,7 +6,7 @@ cmake ^
     -Bbuild ^
     -GNinja ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_CXX_COMPILER:STRING=clang-cl ^
     -DPython_EXECUTABLE:STRING="%PYTHON%" ^
@@ -20,13 +20,15 @@ if errorlevel 1 exit 1
 :: test!
 set KMP_DUPLICATE_LIB_OK=TRUE
 :: we only run unit tests here, integration tests are run later on
-cd build
+cd %SRC_DIR%\build
 ctest -L unit --output-on-failure --parallel %CPU_COUNT%
+cd %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: install!
-cd ..
 cmake --build build --config Release --target install
+:: copy .lib file manually
+copy %SRC_DIR%\build\Lib\site-packages\veloxchem\veloxchemlib.lib %SP_DIR%\veloxchem\veloxchemlib.lib
 if errorlevel 1 exit 1
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
