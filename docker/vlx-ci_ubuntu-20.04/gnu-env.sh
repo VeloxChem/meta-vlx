7aeb4d122914ec51d64569f8e254e2efd07a63d5
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-03-17 16:29:59 +0100

    Rework environment setup scripts

--- docker/vlx-ci_ubuntu-20.04/gnu-env.sh
+++ docker/vlx-ci_ubuntu-20.04/gnu-env.sh
@@ -3,7 +3,5 @@
 printf "default=exclude\n" > config.txt
 source /opt/intel/oneapi/setvars.sh --config=config.txt
 
-# install mpi4py using GNU and OpenMPI
-CC=gcc MPICC=mpicc python -m pip install mpi4py --no-binary=mpi4py
-
-python -c "import mpi4py; print(mpi4py.get_config())"
+export CC=gcc
+export MPICC=mpicc
--- docker/vlx-ci_ubuntu-20.04/intel-env.sh
+++ docker/vlx-ci_ubuntu-20.04/intel-env.sh
@@ -3,7 +3,5 @@
 printf "default=exclude\nmkl=latest\nmpi=latest\ncompiler=latest\n" > config.txt
 source /opt/intel/oneapi/setvars.sh --config=config.txt
 
-# install mpi4py using IntelMPI
-CC=icc MPICC=mpiicc python -m pip install mpi4py --no-binary=mpi4py
-
-python -c "import mpi4py; print(mpi4py.get_config())"
+export CC=icc
+export MPICC=mpiicc
