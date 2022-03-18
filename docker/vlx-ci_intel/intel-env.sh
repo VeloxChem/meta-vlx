d54d2a68599b27db87bdf271a26d2db3016ad922
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-03-18 20:04:06 +0100

    Add entrypoint for intel image

---
+++ docker/vlx-ci_intel/intel-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+printf "default=exclude\nmkl=latest\nmpi=latest\ncompiler=latest\n" > config.txt
+source /opt/intel/oneapi/setvars.sh --config=config.txt
+
+export CC=icc
+export MPICC=mpiicc
+
+exec "$@"
