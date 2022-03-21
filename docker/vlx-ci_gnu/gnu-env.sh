60f6181741abc6c9b852c1dd3914ead47f3865e5
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-03-21 20:51:44 +0100

    Finesse GNU Docker image

--- docker/vlx-ci_gnu/gnu-env.sh
+++ docker/vlx-ci_gnu/gnu-env.sh
@@ -3,6 +3,11 @@
 export CC=gcc
 export MPICC=mpicc
 
+export OPENBLAS_INCLUDE_DIR="/usr/include/x86_64-linux-gnu/openblas-openmp"
+export OPENBLAS_LIBRARY="/usr/lib/x86_64-linux-gnu/openblas-openmp"
 export GTESTLIB="/usr/lib/x86_64-linux-gnu"
+
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
 exec "$@"
