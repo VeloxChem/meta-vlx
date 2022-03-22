3e15b69508beef2ebffedf854723e265e6a203ed
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-03-22 09:02:11 +0100

    Don't use the Intel Python distribution
    It adds ~3GB to the image!

--- docker/vlx-ci_intel/intel-env.sh
+++ docker/vlx-ci_intel/intel-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printf "default=exclude\nmkl=latest\nmpi=latest\ncompiler=latest\nintelpython=latest\n" > config.txt
+printf "default=exclude\nmkl=latest\nmpi=latest\ncompiler=latest\n" > config.txt
 source /opt/intel/oneapi/setvars.sh --config=config.txt
 
 export CC=icc
