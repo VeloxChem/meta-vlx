27aaa79918f952024aba2d506653c701e897ed2c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-04-13 18:42:57 +0200

    Update recipe

--- conda-recipes/veloxchem/build.sh
+++ conda-recipes/veloxchem/build.sh
@@ -15,17 +15,19 @@ cmake \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
     -DCMAKE_CXX_COMPILER:STRING="${CXX}" \
-    -DENABLE_ARCH_FLAGS=OFF \
+    -DCMAKE_C_COMPILER:STRING="${CC}" \
+    -DENABLE_ARCH_FLAGS:BOOL=OFF \
+    -DVLX_LA_VENDOR:STRING="Generic" \
     -DPython_EXECUTABLE:STRING="${PYTHON}" \
     -DPYMOD_INSTALL_FULLDIR:PATH="${SP_DIR#$PREFIX/}/veloxchem"
 
 # build!
-cmake --build build --parallel ${CPU_COUNT} -- -v -d stats
+cmake --build build --parallel "${CPU_COUNT}" -- -v -d stats
 
 # test!
 # we only run unit tests here, integration tests are run later on
 cd build
-ctest -L unit --output-on-failure --parallel ${CPU_COUNT}
+ctest -L unit --output-on-failure --parallel "${CPU_COUNT}"
 
 # install!
 cd ..
