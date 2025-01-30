33e7ce1406adf83ff06171acb6c5c482d52784e4
Xin Li <lixin.reco@gmail.com>
2025-01-30 16:35:24 +0100

    updated docker/vlx-ci_gnu/gnu-env.sh

--- docker/vlx-ci_gnu/gnu-env.sh
+++ docker/vlx-ci_gnu/gnu-env.sh
@@ -6,7 +6,7 @@ export MPICC=mpicc
 export OPENBLAS_INCLUDE_DIR="/usr/include/x86_64-linux-gnu/openblas-openmp"
 export OPENBLAS_LIBRARY="/usr/lib/x86_64-linux-gnu/openblas-openmp"
 export GTESTLIB="/usr/lib/x86_64-linux-gnu"
-export XTBHOME="/usr/local"
+export DFTD4_HOME="/usr/local"
 export LibXC_DIR="/usr/local/lib/cmake/Libxc"
 
 exec "$@"
