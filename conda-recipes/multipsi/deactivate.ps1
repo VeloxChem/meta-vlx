f9716359e84db6eaf32b7ab9bb4c8cc329841aa4
Xin Li <lixin.reco@gmail.com>
2026-05-04 14:27:02 +0200

    Update the handling of PYTHONUTF8 for multipsi on Windows

--- conda-recipes/multipsi/deactivate.ps1
+++ conda-recipes/multipsi/deactivate.ps1
@@ -1,1 +1,11 @@
 $env:KMP_DUPLICATE_LIB_OK="FALSE"
+if (Test-Path Env:_OLD_PYTHONUTF8) {
+    if ($null -ne $env:_OLD_PYTHONUTF8) {
+        $env:PYTHONUTF8 = $env:_OLD_PYTHONUTF8
+    } else {
+        Remove-Item Env:PYTHONUTF8 -ErrorAction SilentlyContinue
+    }
+    Remove-Item Env:_OLD_PYTHONUTF8 -ErrorAction SilentlyContinue
+} else {
+    Remove-Item Env:PYTHONUTF8 -ErrorAction SilentlyContinue
+}
