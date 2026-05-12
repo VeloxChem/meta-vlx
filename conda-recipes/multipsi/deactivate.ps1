b851eef31ec09498ee408b42a2d46d0ff9a023d0
Xin Li <lixin.reco@gmail.com>
2026-05-12 09:47:00 +0200

    Update activate and deactivate scripts

--- conda-recipes/multipsi/deactivate.ps1
+++ conda-recipes/multipsi/deactivate.ps1
@@ -1,11 +1,20 @@
-$env:KMP_DUPLICATE_LIB_OK="FALSE"
-if (Test-Path Env:_OLD_PYTHONUTF8) {
-    if ($null -ne $env:_OLD_PYTHONUTF8) {
-        $env:PYTHONUTF8 = $env:_OLD_PYTHONUTF8
+if (Test-Path Env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK) {
+    if ($null -ne $env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK) {
+        $env:KMP_DUPLICATE_LIB_OK = $env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK
+    } else {
+        Remove-Item Env:KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+    }
+    Remove-Item Env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+} else {
+    Remove-Item Env:KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+}
+if (Test-Path Env:_CONDA_BACKUP_PYTHONUTF8) {
+    if ($null -ne $env:_CONDA_BACKUP_PYTHONUTF8) {
+        $env:PYTHONUTF8 = $env:_CONDA_BACKUP_PYTHONUTF8
     } else {
         Remove-Item Env:PYTHONUTF8 -ErrorAction SilentlyContinue
     }
-    Remove-Item Env:_OLD_PYTHONUTF8 -ErrorAction SilentlyContinue
+    Remove-Item Env:_CONDA_BACKUP_PYTHONUTF8 -ErrorAction SilentlyContinue
 } else {
     Remove-Item Env:PYTHONUTF8 -ErrorAction SilentlyContinue
 }
--- conda-recipes/veloxchem/deactivate.ps1
+++ conda-recipes/veloxchem/deactivate.ps1
@@ -1,1 +1,10 @@
-$env:KMP_DUPLICATE_LIB_OK="FALSE"
+if (Test-Path Env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK) {
+    if ($null -ne $env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK) {
+        $env:KMP_DUPLICATE_LIB_OK = $env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK
+    } else {
+        Remove-Item Env:KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+    }
+    Remove-Item Env:_CONDA_BACKUP_KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+} else {
+    Remove-Item Env:KMP_DUPLICATE_LIB_OK -ErrorAction SilentlyContinue
+}
